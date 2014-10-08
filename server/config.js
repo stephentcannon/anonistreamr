c2acff7f13607331c0f025eb99164e951e270c78
Stephen T Cannon <stephentcannon@gmail.com>
2014-10-07 21:12:29 -0400

    removing old pre 0.6.0 stuff

--- server/config.js
+++ server/config.js
@@ -1,30 +1,22 @@
-var config = {}
+config = {}
 
-config.post_social = false; 
+config.post_social = false;
 
-config.email_to = ''; 
-config.email_from = ''; 
-config.email_bcc = ''; 
+config.email_to = '';
+config.email_from = '';
+config.email_bcc = '';
 
 config.fb = {};
-config.fb.client_id = ''; 
-config.fb.access_token = ''; 
+config.fb.client_id = '';
+config.fb.access_token = '';
 config.fb.page_url = '';
 config.fb.page_id = '';
 
 config.twit = {};
 
 config.twit.options = {
-  consumer_key:         '', 
+  consumer_key:         '',
   consumer_secret:      '',
   access_token:         '',
   access_token_secret:  ''
 }
-
-config.emailer = {};
-config.emailer.service = 'Gmail';
-config.emailer.user = '';
-config.emailer.pass = '';
-config.emailer.host = '';
-config.emailer.secureConnection = true; 
-config.emailer.port = 465; 
